42561ceac5c7d57400b389bb80a3443447ece47e
ping <dev@guillaumepin.ch>
2021-04-30 18:56:40 +0200

    remplace return by exit && remplace dup by dup2

--- src/jobs.c
+++ src/jobs.c
@@ -16,11 +16,11 @@ int execCommand(struct command cmd) {
         int out = open("/dev/null", O_WRONLY, 0640);
         if (out == -1) {
           fprintf(stderr, "open: %s\n", strerror(errno)); 
-          return -1;
+          exit(EXIT_FAILURE);
         }
         if (dup2(out, STDIN_FILENO) == -1) {
           fprintf(stderr, "dup2: %s\n", strerror(errno)); 
-          return -1;
+          exit(EXIT_FAILURE);
         }
       }
       if (cmd.output != NULL) {
@@ -28,27 +28,28 @@ int execCommand(struct command cmd) {
         int out = open(cmd.output, O_WRONLY | O_CREAT | O_TRUNC, 0640);
         if (dup2(out, STDOUT_FILENO) == -1) {
           fprintf(stderr, "dup2: %s\n", strerror(errno)); 
-          return -1;
+          exit(EXIT_FAILURE);
         }
         if (close(out) == -1) {
           fprintf(stderr, "close: %s\n", strerror(errno)); 
-          return -1;
+          exit(EXIT_FAILURE);
         }
       }
       // tcgetpgrp TODO
       if (setpgid(getpid(), getpid()) != 0) {
         fprintf(stderr, "pingshell: %s: setpgid error\n", strerror(errno));
+        exit(EXIT_FAILURE);
       }
       if (execvp(cmd.argv[0], cmd.argv) == -1) { 
         fprintf(stderr, "pingshell: %s: command not found\n", cmd.argv[0]);
-        kill(getpid(), SIGHUP);
-        return -1;
+        exit(EXIT_FAILURE);
       }
     } else {
       if (!cmd.background_task) {
         fg_pid = pid;
         if (waitpid(pid, &status, 0) == -1) {
           fprintf(stderr, "waitpid : %s\n", strerror(errno));
+          return -1;
         } else {
           fprintf(stdout, "Foreground job exited with code [%d] %d\n", pid, status);
         }
@@ -57,7 +58,6 @@ int execCommand(struct command cmd) {
         pid_chld = pid;
         fprintf(stdout, "[1] %d\n", pid_chld);
       }
-      return 0;
     }
   } else {
     int p[2];
@@ -73,29 +73,25 @@ int execCommand(struct command cmd) {
       fprintf(stderr, "fork 1 = %s\n", strerror(errno));
       return -1;
     } else if (p1 == 0) {
-      if (close(STDOUT_FILENO) == -1) { 
-        fprintf(stderr, "close: %s\n", strerror(errno));
-        return -1;
-      }
-      if (dup(p[1]) == -1) {
+      if (dup2(p[1], STDOUT_FILENO) == -1) {
         fprintf(stderr, "dup: %s\n", strerror(errno));
-        return -1;
+        exit(EXIT_FAILURE);
       }
       if (close(p[0]) == -1) {
         fprintf(stderr, "close: %s\n", strerror(errno));
-        return -1;
+        exit(EXIT_FAILURE);
       }
       if (close(p[1]) == -1) {
         fprintf(stderr, "close: %s\n", strerror(errno));
-        return -1;
+        exit(EXIT_FAILURE);
       }
       if (setpgid(getpid(), getpid()) != 0) {
         fprintf(stderr, "pingshell: %s: setpgid error\n", strerror(errno));
+        exit(EXIT_FAILURE);
       }
       if (execvp(cmd.argv[0], cmd.argv) == -1) {
-        fprintf(stderr, "execvp = %s\n", strerror(errno));
-        fprintf(stderr, "pingshell: %s: command not found\n",  cmd.argv[0]);
-        return -1;
+        fprintf(stderr, "pingshell: %s: command not found, error : %s\n",  cmd.argv[0], strerror(errno));
+        exit(EXIT_FAILURE);
       }
     } else {
       p2 = fork();
@@ -103,29 +99,25 @@ int execCommand(struct command cmd) {
         fprintf(stderr, "fork 1 = %s\n", strerror(errno));
         return -1;
       } else if (p2 == 0) {
-        if (close(STDIN_FILENO) == -1) {
-          fprintf(stderr, "close: %s\n", strerror(errno));
-          return -1;
-        }
-        if (dup(p[0]) == -1) {
+        if (dup2(p[0], STDIN_FILENO) == -1) {
           fprintf(stderr, "dup: %s\n", strerror(errno));
-          return -1;
+          exit(EXIT_FAILURE);
         }
         if (close(p[1]) == -1) {
           fprintf(stderr, "close: %s\n", strerror(errno));
-          return -1;
+          exit(EXIT_FAILURE);
         }
         if (close(p[0]) == -1) {
           fprintf(stderr, "close: %s\n", strerror(errno));
-          return -1;
+          exit(EXIT_FAILURE);
         }
         if (setpgid(getpid(), p1) != 0) {
           fprintf(stderr, "pingshell: %s: setpgid error\n", strerror(errno));
+          exit(EXIT_FAILURE);
         }
         if (execvp(cmd.next_command->argv[0], cmd.next_command->argv) == -1) {
-          fprintf(stderr, "execvp = %s\n", strerror(errno));
-          fprintf(stderr, "pingshell: %s: command not found\n",  cmd.argv[0]);
-          return -1;
+          fprintf(stderr, "pingshell: %s: command not found, error : %s\n",  cmd.next_command->argv[0], strerror(errno));
+          exit(EXIT_FAILURE);
         }
       }
       if (close(p[0]) == -1) {
@@ -139,6 +131,7 @@ int execCommand(struct command cmd) {
       fg_pid = p1;
       if (waitpid(p1, &status, 0) == -1) {
         fprintf(stderr, "waitpid : %s\n", strerror(errno));
+        return -1;
       } else {
         fprintf(stdout, "Foreground job [1] %d exited with code %d\n", p1, status);
       }
@@ -147,7 +140,6 @@ int execCommand(struct command cmd) {
       } else {
         fprintf(stdout, "Foreground job [2] %d exited with code %d\n", p2, status);
       }
-      return 0;
     }
   }
   return 0;
